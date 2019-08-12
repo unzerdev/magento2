7ae8c84d2ef22d3bbc3c8a0448d655b534044364
Alexander Kotlear <alexander.kotlear@hmmh.de>
2019-08-12 09:23:54 +0200

    Update di.xml with virtualType

--- Model/Logger/DebugHandler.php
+++ Model/Logger/DebugHandler.php
@@ -3,6 +3,7 @@
 namespace Heidelpay\Gateway2\Model\Logger;
 
 use heidelpayPHP\Interfaces\DebugHandlerInterface;
+//use \Psr\Log\LoggerInterface;
 
 class DebugHandler implements DebugHandlerInterface
 {
@@ -16,7 +17,9 @@ class DebugHandler implements DebugHandlerInterface
      *
      * @param LoggerInterface $logger
      */
-    public function __construct(Logger $logger)
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger
+    )
     {
         $this->_logger = $logger;
     }
@@ -30,6 +33,6 @@ class DebugHandler implements DebugHandlerInterface
      */
     public function log(string $message)
     {
-        $this->_logger->debug(var_export($message, true));
+        $this->_logger->debug($message);
     }
 }
