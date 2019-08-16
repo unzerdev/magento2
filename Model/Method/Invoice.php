27e5e4e966c5f98a544c6d70970215a16bbb174c
Justin Nuß <justin.nuss@hmmh.de>
2019-08-16 10:29:29 +0200

    Fix invoice payment information generation

--- Model/Method/Invoice.php
+++ Model/Method/Invoice.php
@@ -2,20 +2,90 @@
 
 namespace Heidelpay\Gateway2\Model\Method;
 
+use Heidelpay\Gateway2\Model\Config;
 use heidelpayPHP\Resources\Payment;
 use heidelpayPHP\Resources\TransactionTypes\Charge;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Event\ManagerInterface;
 use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Payment\Gateway\Command\CommandManagerInterface;
+use Magento\Payment\Gateway\Command\CommandPoolInterface;
+use Magento\Payment\Gateway\Config\ValueHandlerPoolInterface;
+use Magento\Payment\Gateway\Data\PaymentDataObjectFactory;
+use Magento\Payment\Gateway\Validator\ValidatorPoolInterface;
 use Magento\Sales\Model\Order;
+use Psr\Log\LoggerInterface;
 
 class Invoice extends Base
 {
+    /**
+     * @var Config
+     */
+    protected $_moduleConfig;
+
+    /**
+     * @var PriceCurrencyInterface
+     */
+    protected $_priceCurrency;
+
+    /**
+     * Base constructor.
+     * @param ManagerInterface $eventManager
+     * @param ValueHandlerPoolInterface $valueHandlerPool
+     * @param PaymentDataObjectFactory $paymentDataObjectFactory
+     * @param string $code
+     * @param string $formBlockType
+     * @param string $infoBlockType
+     * @param ScopeConfigInterface $scopeConfig
+     * @param Config $moduleConfig
+     * @param PriceCurrencyInterface $priceCurrency
+     * @param CommandPoolInterface|null $commandPool
+     * @param ValidatorPoolInterface|null $validatorPool
+     * @param CommandManagerInterface|null $commandExecutor
+     * @param LoggerInterface|null $logger
+     */
+    public function __construct(
+        ManagerInterface $eventManager,
+        ValueHandlerPoolInterface $valueHandlerPool,
+        PaymentDataObjectFactory $paymentDataObjectFactory,
+        $code,
+        $formBlockType,
+        $infoBlockType,
+        ScopeConfigInterface $scopeConfig,
+        Config $moduleConfig,
+        PriceCurrencyInterface $priceCurrency,
+        CommandPoolInterface $commandPool = null,
+        ValidatorPoolInterface $validatorPool = null,
+        CommandManagerInterface $commandExecutor = null,
+        LoggerInterface $logger = null
+    ) {
+        parent::__construct(
+            $eventManager,
+            $valueHandlerPool,
+            $paymentDataObjectFactory,
+            $code,
+            $formBlockType,
+            $infoBlockType,
+            $scopeConfig,
+            $commandPool,
+            $validatorPool,
+            $commandExecutor,
+            $logger
+        );
+
+        $this->_moduleConfig = $moduleConfig;
+        $this->_priceCurrency = $priceCurrency;
+    }
+
     /**
      * @inheritDoc
      */
     public function getAdditionalPaymentInformation(Order $order): string
     {
         /** @var Payment $payment */
-        $payment = $this->_getClient()->fetchPaymentByOrderId($order->getIncrementId());
+        $payment = $this->_moduleConfig
+            ->getHeidelpayClient()
+            ->fetchPaymentByOrderId($order->getIncrementId());
 
         /** @var Charge|null $charge */
         $charge = $payment->getChargeByIndex(0);
