89204c0b032dbd1db80d336d1146ff02a034959f
Boljanovic <aleksandar.boljanovic@logeecom.com>
2025-06-13 09:44:22 +0200

    Implement iDEAL for v2 components
    ISSUE: UM2MTVCP-7

--- view/frontend/web/js/view/payment/method-renderer/ideal.js
+++ view/frontend/web/js/view/payment/method-renderer/ideal.js
@@ -1,9 +1,20 @@
 define(
     [
+        'jquery',
         'ko',
-        'Unzer_PAPI/js/view/payment/method-renderer/base'
+        'mage/translate',
+        'Magento_Checkout/js/action/place-order',
+        'Magento_Ui/js/model/messageList',
+        'Unzer_PAPI/js/view/payment/method-renderer/basev2'
     ],
-    function (ko, Component) {
+    function (
+        $,
+        ko,
+        $t,
+        placeOrderAction,
+        globalMessageList,
+        Component
+    ) {
         'use strict';
 
         return Component.extend({
@@ -12,28 +23,52 @@ define(
                 template: 'Unzer_PAPI/payment/ideal'
             },
 
-            initializeForm: function () {
-                const self = this;
+            createSpecificPaymentElement: function () {
+                return $('<unzer-ideal>');
+            },
 
-                this.resourceProvider = this.sdk.Ideal();
-                this.resourceProvider.create('ideal', {
-                    containerId: 'unzer-ideal-field'
+            getPlaceOrderDeferredObject: function () {
+                let deferred = $.Deferred(),
+                    self = this;
+
+                Promise.all([
+                    customElements.whenDefined('unzer-ideal')
+                ]).then(() => {
+                    const unzerCheckoutElementId = 'unzer-checkout-' + this.getCode();
+                    const unzerCheckout = document.getElementById(unzerCheckoutElementId);
+                    unzerCheckout.onPaymentSubmit = response => {
+                        if (response.submitResponse && response.submitResponse.success)  {
+                            this.resourceId = response.submitResponse.data.id;
+                            placeOrderAction(self.getData(), self.messageContainer)
+                                .done(function () {
+                                    deferred.resolve.apply(deferred, arguments);
+                                })
+                                .fail(function (request) {
+                                    globalMessageList.addErrorMessage({
+                                        message: request.responseJSON.message
+                                    });
+                                    deferred.reject(request.responseJSON.message);
+                                });
+                        } else {
+                            globalMessageList.addErrorMessage({
+                                message: 'There was an error placing your order. ' + response.submitResponse.message
+                            });
+                            deferred.reject($t('There was an error placing your order. ' + response.submitResponse.message));
+                        }
+                    };
+                }).catch(error => {
+                    globalMessageList.addErrorMessage({
+                        message: 'There was an error placing your order. ' + error
+                    });
+                    deferred.reject($t('There was an error placing your order. ' + error));
                 });
 
-                this.field.valid = ko.observable(false);
-
-                this.resourceProvider.addEventListener('change', function (event) {
-                    self.field.valid(!!event.value);
+                return deferred.fail(function (error) {
+                    globalMessageList.addErrorMessage({
+                        message: error
+                    });
                 });
-            },
-
-            allInputsValid: function () {
-                return this.field.valid();
-            },
-
-            validate: function () {
-                return this.allInputsValid();
-            },
+            }
         });
     }
 );
