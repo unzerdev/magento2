02dbdcd919962c5f2aa23b9cdddd18f31e6509b8
Boljanovic <aleksandar.boljanovic@logeecom.com>
2025-06-17 13:54:25 +0200

    Fix Prepayment flow

--- view/frontend/web/js/view/payment/method-renderer/prepayment.js
+++ view/frontend/web/js/view/payment/method-renderer/prepayment.js
@@ -3,11 +3,6 @@ define(
         'Unzer_PAPI/js/view/payment/method-renderer/basev2'
     ],
     function (
-        $,
-        ko,
-        $t,
-        placeOrderAction,
-        globalMessageList,
         Component
     ) {
         'use strict';
