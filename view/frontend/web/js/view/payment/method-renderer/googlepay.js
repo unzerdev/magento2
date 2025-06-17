cc3e7a48341257ec6370f6c83d9e8d8253fb5616
Boljanovic <aleksandar.boljanovic@logeecom.com>
2025-06-17 13:52:53 +0200

    Fix GooglePay flow

--- view/frontend/web/js/view/payment/method-renderer/googlepay.js
+++ view/frontend/web/js/view/payment/method-renderer/googlepay.js
@@ -1,10 +1,18 @@
 define(
     [
+        'jquery',
+        'ko',
+        'mage/translate',
+        'Magento_Ui/js/model/messageList',
         'Magento_Checkout/js/action/place-order',
         'Unzer_PAPI/js/view/payment/method-renderer/basev2',
         'Magento_Checkout/js/model/quote'
     ],
     function (
+        $,
+        ko,
+        $t,
+        globalMessageList,
         placeOrderAction,
         Component,
         quote
@@ -21,40 +29,86 @@ define(
             selectPaymentMethod: function () {
                 let retVal = this._super();
 
-                const unzerPaymentElement = document.getElementById('unzer-payment-unzer_googlepay');
-                unzerPaymentElement.setGooglePayData({
-                    gatewayMerchantId: this._getMethodConfig('unzer_channel_id'),
-                    merchantInfo: {
-                        merchantId: this._getMethodConfig('merchant_id'),
-                        merchantName: this._getMethodConfig('merchant_name')
-                    },
-                    transactionInfo: {
-                        countryCode: this._getMethodConfig('country_code'),
-                        currencyCode: (quote.totals() ? quote.totals() : quote)['quote_currency_code'],
-                        totalPrice: String((quote.totals() ? quote.totals() : quote)['grand_total'])
-                    },
-                    buttonOptions: {
-                        buttonColor: this._getMethodConfig('button_color'),
-                        buttonRadius: this._getMethodConfig('button_border_radius'),
-                        buttonSizeMode: this._getMethodConfig('button_size_mode'),
-                    },
-                    allowedCardNetworks: this._getMethodConfig('allowed_card_networks'),
-                    allowCreditCards: this._getMethodConfig('allow_credit_cards') === "1",
-                    allowPrepaidCards: this._getMethodConfig('allow_prepaid_cards') === "1"
-                });
-
+                this.waitForSetGooglePayData();
                 const unzerCheckoutElementId = 'unzer-checkout-' + this.getCode();
                 const unzerCheckout = document.getElementById(unzerCheckoutElementId);
                 unzerCheckout.onPaymentSubmit = response => {
-                    const result = this.placeOrder();
-                    if (result) {
-                        return {status: 'success'};
-                    } else {
-                        return {status: 'error', message: 'Unexpected error'}
+                    if (response.submitResponse && response.submitResponse.success) {
+                        this.resourceId = response.submitResponse.data.id;
+                        const result = this.placeOrder();
+                        if (result) {
+                            return {status: 'success'};
+                        }
                     }
+
+                    return {status: 'error', message: 'Unexpected error'}
+
                 }
                 return retVal;
             },
+
+            waitForSetGooglePayData: function (maxRetries = 10, interval = 500) {
+                const unzerPaymentElement = document.getElementById('unzer-payment-unzer_googlepay');
+
+                if (unzerPaymentElement && typeof unzerPaymentElement.setGooglePayData === 'function') {
+                    unzerPaymentElement.setGooglePayData({
+                        gatewayMerchantId: this._getMethodConfig('unzer_channel_id'),
+                        merchantInfo: {
+                            merchantId: this._getMethodConfig('merchant_id'),
+                            merchantName: this._getMethodConfig('merchant_name')
+                        },
+                        transactionInfo: {
+                            countryCode: this._getMethodConfig('country_code'),
+                            currencyCode: (quote.totals() ? quote.totals() : quote)['quote_currency_code'],
+                            totalPrice: String((quote.totals() ? quote.totals() : quote)['grand_total'])
+                        },
+                        buttonOptions: {
+                            buttonColor: this._getMethodConfig('button_color'),
+                            buttonRadius: this._getMethodConfig('button_border_radius'),
+                            buttonSizeMode: this._getMethodConfig('button_size_mode'),
+                        },
+                        allowedCardNetworks: this._getMethodConfig('allowed_card_networks'),
+                        allowCreditCards: this._getMethodConfig('allow_credit_cards') === "1",
+                        allowPrepaidCards: this._getMethodConfig('allow_prepaid_cards') === "1"
+                    });
+                } else if (maxRetries > 0) {
+                    console.log('Waiting for setGooglePayData function to be available...');
+                    setTimeout(() => this.waitForSetGooglePayData(maxRetries - 1, interval), interval);
+                } else {
+                    console.error('setGooglePayData is not available after multiple retries.');
+                }
+            },
+
+            getPlaceOrderDeferredObject: function () {
+                let deferred = $.Deferred(),
+                    self = this;
+
+                Promise.all([
+                    customElements.whenDefined(this.paymentCode)
+                ]).then(() => {
+                    placeOrderAction(self.getData(), self.messageContainer)
+                        .done(function () {
+                            deferred.resolve.apply(deferred, arguments);
+                        })
+                        .fail(function (request) {
+                            globalMessageList.addErrorMessage({
+                                message: request.responseJSON.message
+                            });
+                            deferred.reject(request.responseJSON.message);
+                        });
+                }).catch(error => {
+                    globalMessageList.addErrorMessage({
+                        message: 'There was an error placing your order. ' + error
+                    });
+                    deferred.reject($t('There was an error placing your order. ' + error));
+                });
+
+                return deferred.fail(function (error) {
+                    globalMessageList.addErrorMessage({
+                        message: error
+                    });
+                });
+            }
         });
     }
 );
