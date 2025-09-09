c310200abc1f1c29035cbae6089d31866d0f93b9
Kristina <kristina.mutavdzic@logeecom.com>
2025-09-09 14:50:54 +0200

    Fix missing edit billing address option on checkout and amount discrepancy with Apple Pay
    ISSUE: MAGENTO-316,MAGENTO-321

--- view/frontend/web/js/view/payment/method-renderer/applepayv2.js
+++ view/frontend/web/js/view/payment/method-renderer/applepayv2.js
@@ -25,6 +25,10 @@ define(
                 paymentCode: 'unzer-apple-pay'
             },
 
+            isApplePayAvailable: function () {
+                return window.ApplePaySession && ApplePaySession.canMakePayments();
+            },
+
             selectPaymentMethod: function () {
                 let retVal = this._super();
 
@@ -52,18 +56,20 @@ define(
                 if (unzerPaymentElement && typeof unzerPaymentElement.setApplePayData === 'function') {
                     const supportedNetworks = window.checkoutConfig.payment.unzer_applepayv2.supportedNetworks.map((network) => network.toLowerCase());
 
+                    const totals = quote.totals() ? quote.totals() : window.checkoutConfig.quoteData;
+
                     unzerPaymentElement.setApplePayData({
                         countryCode: quote.billingAddress().countryId,
                         currencyCode: window.checkoutConfig.quoteData.quote_currency_code,
                         totalLabel: window.checkoutConfig.payment.unzer_applepayv2.label,
-                        totalAmount: Number(window.checkoutConfig.quoteData.base_grand_total).toFixed(2),
+                        totalAmount:  Number(totals['grand_total']).toFixed(2),
                         supportedNetworks: supportedNetworks,
                         merchantCapabilities: window.checkoutConfig.payment.unzer_applepayv2.merchantCapabilities,
                         requiredShippingContactFields: [],
                         requiredBillingContactFields: [],
                         total: {
                             label: window.checkoutConfig.payment.unzer_applepayv2.label,
-                            amount: Number(window.checkoutConfig.quoteData.base_grand_total).toFixed(2)
+                            amount: Number(totals['grand_total']).toFixed(2),
                         },
                     });
                 } else if (maxRetries > 0) {
