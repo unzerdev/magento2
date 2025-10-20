b1ea15ea0bb185ad81513ff62ec6ad2e90cc49d4
Kristina <kristina.mutavdzic@logeecom.com>
2025-10-20 10:18:40 +0200

    Fix wero.js
    ISSUE: MAGENTO-299

--- view/frontend/web/js/view/payment/method-renderer/wero.js
+++ view/frontend/web/js/view/payment/method-renderer/wero.js
@@ -1,17 +1,16 @@
-define([
-    'jquery',
-    'ko',
-    'mage/translate',
-    'Magento_Checkout/js/action/place-order',
-    'Magento_Ui/js/model/messageList',
-    'Unzer_PAPI/js/view/payment/method-renderer/basev2'
-], function ($, ko, $t, placeOrderAction, globalMessageList, Component) {
-    'use strict';
+define(
+    [
+        'Unzer_PAPI/js/view/payment/method-renderer/basev2'
+    ],
+    function (
+        Component
+    ) {
+        'use strict';
 
-    return Component.extend({
-        defaults: {
-            isActivePaymentTokenEnabler: false,
-            template: 'Unzer_PAPI/payment/wero'
-        }
+        return Component.extend({
+            defaults: {
+                template: 'Unzer_PAPI/payment/wero',
+                paymentCode: 'unzer-wero',
+            }
+        });
     });
-});
